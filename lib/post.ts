54539c962d8acda30756a3822ed411fa6664a05c
ygvbhy <ygvbhy@naver.com>
2024-11-28 20:54:31 +0900

    getStaticProps를 이용한 포스트 리스트 나열

--- lib/post.ts
+++ lib/post.ts
@@ -15,7 +15,7 @@ export function getSortedPostsData() {
 
     return {
       id,
-      ...allPostsData(matterResult.data as { date: string; title: string }),
+      ...(matterResult.data as { date: string; title: string }),
     };
   });
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import Head from "next/head";
 import homeStyle from "../styles/Home.module.css";
+import { getSortedPostsData } from "@/lib/post";
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
+
+function Home({
+  allPostsData,
+}: {
+  allPostsData: { date: string; title: string; id: string }[];
+}) {
   return (
     <>
       <Head>
@@ -15,9 +29,17 @@ export default function Home() {
       <section className={`${homeStyle.headingMd} ${homeStyle.padding1px}`}>
         <h2 className={homeStyle.headingLg}>Blog</h2>
         <ul className={homeStyle.list}>
-          <li></li>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={homeStyle.listItem} key={id}>
+              <a href="">{title}</a>
+              <br />
+              <small className={homeStyle.lightText}>{date}</small>
+            </li>
+          ))}
         </ul>
       </section>
     </>
   );
 }
+
+export default Home;
