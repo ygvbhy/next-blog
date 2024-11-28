2203a40b932465a163f733cc4271c2b0331168a2
ygvbhy <ygvbhy@naver.com>
2024-11-28 14:21:40 +0900

    마크다운 파일을 데이터로 추출하기

---
+++ lib/post.ts
@@ -0,0 +1,29 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export function getSortedPostsData() {
+  // /posts 파일 이름을 잡아주기
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, "");
+    const fullPath = path.join(postsDirectory, fileName);
+    const filecontents = fs.readFileSync(fullPath, "utf8");
+    const matterResult = matter(filecontents);
+
+    return {
+      id,
+      ...allPostsData(matterResult.data as { date: string; title: string }),
+    };
+  });
+
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
